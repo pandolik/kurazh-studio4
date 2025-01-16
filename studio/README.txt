f39a8d04701e0a01e1999a8a48056c11db852035
Anatolii Kornylenko <98897140+pandolik@users.noreply.github.com>
2025-01-17 01:58:00 +0200

    Delete studio directory

--- studio/README.txt
+++
@@ -1,6 +0,0 @@
-!!!
-Если расширение видео 1080/1920 - DEFAULT
-ELSE:
-	Изменить HEIGHT in ".player__video { height: 30vh !important; }" (style.css)
-
-!!!
--- studio/player/src/app.js
+++
@@ -1,6 +0,0 @@
-import './main.scss';
-
-import Player from './player/player.js';
-import './player/player.scss';
-
-const player = new Player('.j-player');
